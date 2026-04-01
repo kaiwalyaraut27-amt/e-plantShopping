e152f6511edd8862436c3eedc993ca2d36c9c721
kaiwalyaraut27-amt <kaiwalyaraut27@gmail.com>
2026-04-01 19:10:58 +0530

    Update and rename App.js to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,28 @@ import ProductList from "./components/ProductList";
 import CartItem from "./components/CartItem";
 
 function App() {
+  const [showPlants, setShowPlants] = useState(false);
   const [page, setPage] = useState("home");
 
   return (
-    <div className="app">
+    <div>
       <nav>
         <h2>Paradise Nursery 🌿</h2>
-        <button onClick={() => setPage("home")}>Home</button>
-        <button onClick={() => setPage("plants")}>Plants</button>
+        <button onClick={() => {setPage("home"); setShowPlants(false);}}>Home</button>
+        <button onClick={() => {setPage("plants"); setShowPlants(true);}}>Plants</button>
         <button onClick={() => setPage("cart")}>Cart</button>
       </nav>
 
       {page === "home" && (
-        <div className="home">
+        <div className="background-image">
           <h1>Welcome to Paradise Nursery</h1>
-          <button onClick={() => setPage("plants")}>Get Started</button>
+          <button onClick={() => {setShowPlants(true); setPage("plants");}}>
+            Get Started
+          </button>
         </div>
       )}
 
-      {page === "plants" && <ProductList />}
+      {page === "plants" && showPlants && <ProductList />}
       {page === "cart" && <CartItem />}
     </div>
   );
