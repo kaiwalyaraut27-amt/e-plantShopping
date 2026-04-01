505e36741f895cad396fac71e1c419474b56397b
kaiwalyaraut27-amt <kaiwalyaraut27@gmail.com>
2026-04-01 19:11:56 +0530

    Update CartSlice.jsx

--- src/redux/CartSlice.jsx
+++ src/redux/CartSlice.jsx
@@ -3,7 +3,28 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
   name: "cart",
   initialState: { items: [] },
-  reducers: {}
+  reducers: {
+    addItem: (state, action) => {
+      const item = state.items.find(i => i.id === action.payload.id);
+      if (item) {
+        item.quantity++;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
+    },
+
+    removeItem: (state, action) => {
+      state.items = state.items.filter(i => i.id !== action.payload);
+    },
+
+    updateQuantity: (state, action) => {
+      const item = state.items.find(i => i.id === action.payload.id);
+      if (item) {
+        item.quantity = action.payload.quantity;
+      }
+    }
+  }
 });
 
+export const { addItem, removeItem, updateQuantity } = cartSlice.actions;
 export default cartSlice.reducer;
